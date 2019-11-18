6e73b77103d2ac34581178243f97cd2bb451b89a
LenWinkler <lenwinkler86@gmail.com>
2019-11-18 18:04:22 -0500

    made changes for heroku deployment

--- api/server.js
+++ api/server.js
@@ -8,6 +8,10 @@ const driversRouter = require('../drivers/drivers-router.js');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send({ api: "up" })
+});
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
@@ -15,5 +19,6 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/drivers', driversRouter);
+
 
 module.exports = server;
