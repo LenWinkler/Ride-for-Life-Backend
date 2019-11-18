35eda62ccd79c9ab4d66f28d43a3f7155102fe9c
LenWinkler <lenwinkler86@gmail.com>
2019-11-18 04:07:38 -0500

    started writing drivers router

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 const Users = require('./users-model.js');
-const Reviews = require('../reviews/reviews-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
 router.get('/:id',restricted, (req, res) => {
