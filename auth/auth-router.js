d0ec2fcc74f7c1f84cfac60d041cc7182e4785c3
LenWinkler <lenwinkler86@gmail.com>
2019-11-19 11:42:40 -0500

    added id to response for user login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -75,10 +75,11 @@ router.post('/user_login', (req, res) => {
     Users.findBy({ users_email })
         .first()
         .then(user => {
+            let {id} = user;
             if (user && bcrypt.compareSync(password, user.password)) {
                 const token = getJwtTokenUser(user);
 
-                res.status(200).json({ token: token })
+                res.status(200).json({id: id, token: token })
             } else {
                 res.status(400).json({ message: 'Invalid credentials' })
             }
