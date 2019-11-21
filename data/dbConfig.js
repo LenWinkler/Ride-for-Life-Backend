2520345eace13c21d889314ff3a33bc854613486
LenWinkler <lenwinkler86@gmail.com>
2019-11-21 11:59:31 -0500

    wrote tests for login endpoints

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,7 +4,4 @@ require('dotenv').config();
 const config = require('../knexfile.js');
 const environment = process.env.ENVIRONMENT || 'development';
 
-console.log(config);
-console.log(environment);
-
 module.exports = knex(config[environment]);
